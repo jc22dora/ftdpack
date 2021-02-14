5c8d45978d62bca89f7829e1ae13655a2100e059
jc22dora <jc22dora@siena.edu>
2021-02-14 12:39:31 -0500

    update

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup
 setup(name='ftdpack',
 version='0.1',
 description='package to access failure to deliver data',
-url='jdoran.dev',
+url='https://github.com/jc22dora/ftdpack',
 author='John Doran',
 author_email='test@gmail.com',
 license='MIT',
 packages=['ftd'],
+install_requires=['mysql-connector==2.2.9'],
 zip_safe=False)
 
 
