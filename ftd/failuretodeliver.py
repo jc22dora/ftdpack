b4c1564a6687bc9afe9fb02462f7917242da03ac
jc22dora <jc22dora@siena.edu>
2021-02-20 07:27:32 -0500

    official release commit

--- ftd/failuretodeliver.py
+++ ftd/failuretodeliver.py
@@ -2,7 +2,7 @@ import mysql.connector
 import webbrowser
 
 connection = mysql.connector.connect(user='root', password='', host ='104.197.120.134', database='ftdwkey')
-cursor = connection.cursor(dictionary=True)
+cursor = connection.cursor()
 
 def get_access():
     return connection, cursor
@@ -10,3 +10,13 @@ def get_access():
 def download_csv():
     url = 'https://storage.googleapis.com/ftdbucket/ftd/ftdcln.csv'
     return webbrowser.open(url)
+
+def ftd_schema():
+    return cursor.column_names
+
+def most_recent_settlementdate():
+    cursor = connection.cursor(buffered=True, dictionary=True)
+    cursor.execute("SELECT settlementdate FROM ftdwkey ORDER BY settlementdate DESC LIMIT 1")
+    settlementdate = cursor.fetchone()
+    cursor = connection.cursor()
+    return settlementdate['settlementdate']
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='ftdpack',
-version='0.2',
+version='1.0',
 description='package to access failure to deliver data',
 url='https://github.com/jc22dora/ftdpack',
 author='John Doran',
-author_email='test@gmail.com',
+author_email='jdorrkng@jdoran.dev',
 license='MIT',
 packages=['ftd'],
 install_requires=['mysql-connector==2.2.9'],
